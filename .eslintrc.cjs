a3a1242de7af9372e72537101ce23355624e843b
Gustavo Nogales <gustavo.nogales@hotmail.com>
2024-08-03 10:58:15 -0300

    fix: typescript eslint paths resolver

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,7 +6,7 @@ module.exports = {
     'plugin:jsx-a11y/recommended',
     'plugin:@typescript-eslint/recommended',
     'eslint-config-prettier',
-    "plugin:react-hooks/recommended"
+    'plugin:react-hooks/recommended'
   ],
   settings: {
     react: {
@@ -17,16 +17,19 @@ module.exports = {
         paths: ['src'],
         extensions: ['.js', '.jsx', '.ts', '.tsx'],
       },
+      typescript: {
+        project: './tsconfig.paths.json',
+      },
     },
   },
   rules: {
-    "@typescript-eslint/no-explicit-any": "off",
-    "react/react-in-jsx-scope": "off",
-    "import/no-named-as-default": 0,
-    "import/no-unresolved": ["error", { "ignore": ["^@/"] }],
-    "react/display-name": "off",
-    "@typescript-eslint/ban-types": "off",
-    "import/named": "off",
-    "react-hooks/exhaustive-deps": "warn"
+    '@typescript-eslint/no-explicit-any': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'import/no-named-as-default': 0,
+    'import/no-unresolved': ['error', { 'ignore': ['^@/'] }],
+    'react/display-name': 'off',
+    '@typescript-eslint/ban-types': 'off',
+    'import/named': 'off',
+    'react-hooks/exhaustive-deps': 'warn'
   },
 };
